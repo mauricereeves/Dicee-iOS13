11d6ead240334a09bd6e52f0e5e9b5815a258c8e
Maurice Reeves <maurice.reeves@gmail.com>
2020-04-16 15:16:59 -0400

    Wires in the image views and the button click

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -4,17 +4,46 @@
 //
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
-//
+//  Further modified by Maurice Reeves on 16-Apr-2020 learning iOS programming
 
 import UIKit
 
 class ViewController: UIViewController {
+    // make the dice array a constant
+    // I mean, probably okay for it to be
+    // a variable because then we could
+    // change the color of the dice, etc
+    // but for now it's a constant
+    
+    // honestly I would prefer this to be a
+    // map so then I could ref by a number,
+    // or even an enum value
+    let dice: [UIImage] = [
+        #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")
+    ]
+    
+    var leftDiceNumber = 5
+    var rightDiceNumber = 5
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = dice[leftDiceNumber]
+        diceImageView2.image = dice[rightDiceNumber]
+        
+        if (leftDiceNumber > 0 && rightDiceNumber > 0) {
+            leftDiceNumber = leftDiceNumber - 1
+            rightDiceNumber = rightDiceNumber - 1
+        } else {
+            leftDiceNumber = 5
+            rightDiceNumber = 5
+        }
+    }
 }
 
