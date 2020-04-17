94976c32ff3ef1045fdd1c51ecdca8a9e86e1620
Maurice Reeves <maurice.reeves@gmail.com>
2020-04-17 16:06:32 -0400

    Adds randomization and wobble on the dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -21,36 +21,19 @@ class ViewController: UIViewController {
     let dice: [UIImage] = [
         #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")
     ]
-    
-    var leftDiceNumber = 5
-    var rightDiceNumber = 5
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         spinImageView(imageView: diceImageView1, rotateLeft: false)
         spinImageView(imageView: diceImageView2, rotateLeft: true)
-        
-        diceImageView1.image = dice[leftDiceNumber]
-        diceImageView2.image = dice[rightDiceNumber]
-        
-        if (leftDiceNumber > 0 && rightDiceNumber > 0) {
-            leftDiceNumber = leftDiceNumber - 1
-            rightDiceNumber = rightDiceNumber - 1
-        } else {
-            leftDiceNumber = 5
-            rightDiceNumber = 5
-        }
     }
     
     // gives us a method to spin our image view when we "roll" the dice
     func spinImageView(imageView: UIImageView, rotateLeft: Bool = true) {
+        let shakeAmountX: CGFloat = 25
+        let imageViewCenterX: CGFloat = imageView.center.x
         var rotationAmount: CGFloat = 3
         
         if (rotateLeft == false) {
@@ -58,14 +41,28 @@ class ViewController: UIViewController {
         }
         
         UIView.animate(withDuration: 1.0, animations: {
+            imageView.center.x = imageView.center.x + (shakeAmountX * -1)
             imageView.transform = imageView.transform.rotated(by: .pi/rotationAmount)
+            imageView.center.x = imageView.center.x + shakeAmountX
             imageView.transform = imageView.transform.rotated(by: .pi/rotationAmount)
+            imageView.center.x = imageView.center.x + shakeAmountX
             imageView.transform = imageView.transform.rotated(by: .pi/rotationAmount)
+            imageView.center.x = imageView.center.x + (shakeAmountX * -1)
             
             imageView.transform = imageView.transform.rotated(by: .pi/rotationAmount)
+            imageView.center.x = imageView.center.x + shakeAmountX
             imageView.transform = imageView.transform.rotated(by: .pi/rotationAmount)
             imageView.transform = imageView.transform.rotated(by: .pi/rotationAmount)
-        })
+            imageView.center.x = imageView.center.x + (shakeAmountX * -1)
+            
+            imageView.center.x = imageViewCenterX
+        }, completion: setDiceValues)
+    }
+    
+    // set the dice values
+    func setDiceValues(_: Bool) {
+        diceImageView1.image = dice[Int.random(in: 0...5)]
+        diceImageView2.image = dice[Int.random(in: 0...5)]
     }
 }
 
